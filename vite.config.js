18dd9bebc7c2461bca9fd82ee12a28d517f19733
Georgi Bukia <okretans@mail.ru>
2025-05-01 00:34:09 +0400

    Start fixed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // Импортируем функцию defineConfig из Vite
 import { defineConfig } from 'vite'
 // Импортируем плагин React для Vite
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // Экспортируем конфигурацию Vite
 export default defineConfig({
